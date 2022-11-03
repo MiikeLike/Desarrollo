73d698d7b673ebbc99441d8c04c7a641f1fbe0a9
MiikeLike <mak_valle@hotmail.es>
2022-11-03 21:18:00 +0100

    Primera idea

--- Desarrollo/ViewController.swift
+++ Desarrollo/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //TextField
+    
+    @IBOutlet weak var myTextFieldM: UITextField!
+    @IBOutlet weak var myTextFieldMe: UITextField!
+    @IBOutlet weak var myTextFieldN: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myTextFieldM.textColor = .blue
+        myTextFieldM.placeholder = "Escribir diámetro mayor del rollo"
+        myTextFieldM.delegate = self
+        
+        myTextFieldMe.textColor = .blue
+        myTextFieldMe.placeholder = "Escribir diámetro menor del rollo"
+        myTextFieldMe.delegate = self
+        
+        myTextFieldN.textColor = .blue
+        myTextFieldN.placeholder = "Escribir el número de vueltas que tiene el rollo"
+        myTextFieldN.delegate = self
+        
+        
+        
     }
 
 
 }
 
+
+//UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+}
