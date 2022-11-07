a9a366345be685d7c6384b327217b1370fa54706
MiikeLike <mak_valle@hotmail.es>
2022-11-07 21:07:41 +0100

    Implementación numberPad y resignFirstResponder

--- Desarrollo/View/ViewController.swift
+++ Desarrollo/View/ViewController.swift
@@ -39,6 +39,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextFields()
+        configureTapGesture()
    //MARK: -TextField
         myTextFieldM.textColor = .blue
         myTextFieldM.placeholder = "Escribir diámetro mayor del rollo"
@@ -51,38 +53,38 @@ class ViewController: UIViewController {
         myTextFieldN.placeholder = "Escribir el número de vueltas que tiene el rollo"
         myTextFieldN.delegate = self
         
-        myTextFieldM.tag = 0
-        myTextFieldMe.tag = 1
-        myTextFieldN.tag = 2
+        myTextFieldM.keyboardType = .numberPad
+        myTextFieldMe.keyboardType = .numberPad
+        myTextFieldN.keyboardType = .numberPad
         
     }
-
+    private func configureTapGesture(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap))
+            view.addGestureRecognizer(tapGesture)
+    }
+    @objc func handleTap(){
+        print("Toque en la pantalla")
+        view.endEditing(true)
+    }
+    
+    private func configureTextFields(){
+        myTextFieldM.delegate = self
+        myTextFieldN.delegate = self
+        myTextFieldMe.delegate = self
+    }
     
 }
  //MARK: -UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) ->Bool {
-        switch textField.tag{
-        case 0:
-            let caracteres = CharacterSet.alphanumerics
-            let caraterSet = CharacterSet(charactersIn: string)
-            return caracteres.isSuperset(of: caraterSet)
-            
-        case 1:
-            let caracteres = CharacterSet.alphanumerics
-            let caraterSet = CharacterSet(charactersIn: string)
-            return caracteres.isSuperset(of: caraterSet)
-            
-        case 2:
-            let caracteres = CharacterSet.alphanumerics
-            let caraterSet = CharacterSet(charactersIn: string)
-            return caracteres.isSuperset(of: caraterSet)
-            
-        default:
-            print("Fuera de rango")
-        }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
+    
+    
+    
+    
     private func result(){
         
     }
